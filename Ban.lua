d0c32ab5dafda12c565c0b2d414d50fbeeea0914
lucas130919 <cheste1309@gmail.com>
2026-03-24 23:53:57 -0300

    Uncomment ban reason for account selling

--- Ban.lua
+++ Ban.lua
@@ -6,7 +6,7 @@ local Ban = {
 		[3128190107] = {Motivo = 'flw'}, -- ban all
 		[9828582963] = {Motivo = 'Saiba, o que está ruim pode piorar. E ainda não é o pior.'}, -- flw
 		[4209153368] = {Motivo = 'Exploit, quando der like eu desban'}, -- flw
-		--[5677915950] = {Motivo = 'Venda de contas.'}, -- Comprovante Falso.
+		[5677915950] = {Motivo = 'Venda de contas.'}, -- Comprovante Falso.
 		[8200217733] = {Motivo = 'Comprovante Falso'}, -- Comprovante Falso.
 		[8446082243] = {Motivo = 'Troll'}, -- direct ban membros staff
 		[1084069898] = {Motivo = 'Troll'}, -- direct ban membros staff
